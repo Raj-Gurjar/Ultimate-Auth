88051e5c281d40c58c33305aa41cb8dc43ac9613
Raj-Gurjar <20ai1045@mitsgwl.ac.in>
2024-03-01 20:53:22 +0530

    getting usersbyid Successfully

--- server/controllers/getUser.js
+++ server/controllers/getUser.js
@@ -16,8 +16,8 @@ exports.getAllUsers = async (req, res) => {
 
 exports.getUserById = async (req, res) => {
     try {
-        const userId = req.params.id;
-        const getUserById = await userData.findById({ _id: userId });
+        const id = req.params.id;
+        const getUserById = await userData.findById({ _id: id });
 
 
         if (!getUserById) {
@@ -30,14 +30,18 @@ exports.getUserById = async (req, res) => {
         //data for given id FOUND
         res.status(200).json({
             success: true,
-            data: this.getUserById,
+            data: getUserById,
             message: `User ${id} data successfully fetched`,
+           
         })
+        console.log('user by id data :', getUserById);
     }
     catch (error) {
         res.status(500).json({
             success: false,
             message: 'Internal error in user by id'
         })
+
+        console.log('Error in getting user data by id');
     }
 }
